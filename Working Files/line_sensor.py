7f0c9f77e71c7fdff04ec4ed6c10b89cd1d52ab7
malaikajha <mj657@cam.ac.uk>
2025-11-13 10:36:32 +0000

    Update line_sensor.py

--- Working Files/line_sensor.py
+++ Working Files/line_sensor.py
@@ -1,5 +1,6 @@
 from machine import Pin, PWM
 from utime import sleep
+from motor_control import Motor_controller
 
 #Set the line sensor pins
 line_sensor_front_left_pin = 14
@@ -11,56 +12,35 @@ line_sensor_front_right = Pin(line_sensor_front_right_pin, Pin.IN, Pin.PULL_DOWN
 line_sensor_back_left = Pin(line_sensor_back_left_pin, Pin.IN, Pin.PULL_DOWN)
 line_sensor_back_right = Pin(line_sensor_back_right_pin, Pin.IN, Pin.PULL_DOWN) # 0=Black 1=White
 
-class Motor:
-    def __init__(self, dirPin, PWMPin):
-        self.mDir = Pin(dirPin, Pin.OUT)  # set motor direction pin
-        self.pwm = PWM(Pin(PWMPin))  # set motor pwm pin
-        self.pwm.freq(1000)  # set PWM frequency
-        self.pwm.duty_u16(0)  # set duty cycle - 0=off
-        
-    def off(self):
-        self.pwm.duty_u16(0)
-        
-    def Forward(self, speed=100):
-        self.mDir.value(0)                     # forward = 0 reverse = 1 motor
-        self.pwm.duty_u16(int(65535 * speed / 100))  # speed range 0-100 motor
 
-    def Reverse(self, speed=30):
-        self.mDir.value(1)
-        self.pwm.duty_u16(int(65535 * speed / 100))
-
-
-def motor_control():
+def line_sensor_motor_control():
     leftMotor = Motor(dirPin=4, PWMPin=5)  # Motor 3 is controlled from Motor Driv2 #1, which is on GP4/5
     rightMotor = Motor(dirPin=7, PWMPin=6)
+    
+    motor_controller = Motor_controller(4, 5, 7, 6)
 
-    #realign if either sensor is above the white line
-    if line_sensor_back_left = True:
+    #no upcoming corners/turns
+    if line_sensor_front_left = False and line_sensor_front_right = False:
         
-    elif line_sensor_back_right = True:
-
-    #forward movement when both 
-    elif line_sensor_back_left = False and line_sensor
-
-    while True:
-        if forward_button.value() == 1:
-            leftMotor.Forward()
-            rightMotor.Forward()
-            sleep(1)
-            leftMotor.off()
-            rightMotor.off()
-        elif left_button.value() == 0:
-            leftMotor.Reverse()
-            rightMotor.Forward()
-            sleep(0.5)
-            leftMotor.off()
-            rightMotor.off()
-        elif right_button.value() == 0:
-            leftMotor.Forward()
-            rightMotor.Reverse()
-            sleep(0.5)
-            leftMotor.off()
-            rightMotor.off()
-            
-
+        #forward movement when both sensors are either side of white line
+        if line_sensor_back_left = False and line_sensor_back_right = False:
+            motor_controller.move_straight(100)
+        
+        #realign if either sensor is above the white line
+        elif line_sensor_back_left = True:
+            decrease_left_motor_speed(20)
+        
+        elif line_sensor_back_right = True:
+            decrease_right_motor_speed(20)
+         
+        
+    #approaching corner right turn
+    elif line_sensor_front_right = True:
+        decrease_right_motor_speed(100)
+        sleep(1)
+        
+    #approaching corner left turn
+    elif line_sensor_front_left = True:
+        decrease_left_motor_speed(100)
+        sleep(1)
 
