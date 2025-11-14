ed5080c436bba4519267c998105cd1db2fa3ffd5
malaikajha <mj657@cam.ac.uk>
2025-11-14 11:25:10 +0000

    Update line_sensor.py

--- Working Files/line_sensor.py
+++ Working Files/line_sensor.py
@@ -4,34 +4,34 @@ from motor_control import Motor_controller
 from route_planning import Route
 
 #Set the line sensor pins
-line_sensor_front_left_pin = 14
-line_sensor_front_right_pin = 15
-line_sensor_back_left_pin = 16
-line_sensor_back_right_pin = 17
-line_sensor_front_left = Pin(line_sensor_front_left_pin, Pin.IN, Pin.PULL_DOWN)
-line_sensor_front_right = Pin(line_sensor_front_right_pin, Pin.IN, Pin.PULL_DOWN)
-line_sensor_back_left = Pin(line_sensor_back_left_pin, Pin.IN, Pin.PULL_DOWN)
-line_sensor_back_right = Pin(line_sensor_back_right_pin, Pin.IN, Pin.PULL_DOWN) # 0=Black 1=White
+line_sensor_outer_left_pin = 14
+line_sensor_outer_right_pin = 15
+line_sensor_inner_left_pin = 16
+line_sensor_inner_right_pin = 17
+line_sensor_outer_left = Pin(line_sensor_outer_left_pin, Pin.IN, Pin.PULL_DOWN)
+line_sensor_outer_right = Pin(line_sensor_outer_right_pin, Pin.IN, Pin.PULL_DOWN)
+line_sensor_inner_left = Pin(line_sensor_inner_left_pin, Pin.IN, Pin.PULL_DOWN)
+line_sensor_inner_right = Pin(line_sensor_inner_right_pin, Pin.IN, Pin.PULL_DOWN) # 0=Black 1=White
 
 
 def line_sensor_motor_control(motor_controller, route):
 
     #no upcoming corners/turns
-    if line_sensor_front_left.value() == 0 and line_sensor_front_right.value() == 0:
+    if line_sensor_outer_left.value() == 0 and line_sensor_outer_right.value() == 0:
         
         #forward movement when both sensors are either side of white line
-        if line_sensor_back_left.value() == 0 and line_sensor_back_right.value() == 0:
+        if line_sensor_inner_left.value() == 0 and line_sensor_inner_right.value() == 0:
             motor_controller.move_straight(50)
             
-        elif line_sensor_back_left.value() == 1 and line_sensor_back_right.value() == 1:
+        elif line_sensor_inner_left.value() == 1 and line_sensor_inner_right.value() == 1:
             motor_controller.stop()
         
         #realign if either sensor is above the white line
-        elif line_sensor_back_right.value() == 1:
+        elif line_sensor_inner_right.value() == 1:
             motor_controller.set_right_motor_speed(30)
             motor_controller.set_left_motor_speed(50)
         
-        elif line_sensor_back_left.value() == 1:
+        elif line_sensor_inner_left.value() == 1:
             motor_controller.set_left_motor_speed(30)
             motor_controller.set_right_motor_speed(50)
         
