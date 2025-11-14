23ff3d0ba3fb910fae18f0b3fbf468562b45e747
malaikajha <mj657@cam.ac.uk>
2025-11-14 11:45:03 +0000

    button

--- Working Files/main.py
+++ Working Files/main.py
@@ -4,6 +4,10 @@ from line_sensor import line_sensor_motor_control
 from motor_control import Motor_controller
 from route_planning import Route
 
+#Set the button pin
+button_pin = 12
+button = Pin(button_pin, Pin.IN, Pin.PULL_DOWN)
+  
 line_sensor_outer_left_pin = 14
 line_sensor_outer_right_pin = 15
 line_sensor_inner_left_pin = 16
@@ -19,5 +23,15 @@ motor_controller = Motor_controller(4, 5, 7, 6)
 
 route = Route() # initialise default route
 
+
+# Main loop: run until button pressed again
 while True:
+    # If button pressed again → exit
+    if button.value() == 1:
+        break
+
+    # Line following control
     line_sensor_motor_control(motor_controller, route)
+
+# Stop the motors when exiting
+motor_controller.stop()
